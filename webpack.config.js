70d481c4282848e09e6b1b990e7aa622dafa39dc
zmalakhov <zm.13.15.zm@gmail.com>
2019-12-13 11:24:03 +0300

    002 - 2__Полная настройка Webpack 4 препроцессоров. Sass, настройка post css плагинов, минификация стилей. video_2 - 10:29

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: {
@@ -14,9 +15,34 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
+        }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {sourceMap: true}
+                }, {
+                    loader: "sass-loader",
+                    options: {sourceMap: true}
+                }
+
+            ]
+        }, {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ]
         }]
     },
     devServer: {
         overlay: true
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        })
+    ],
 };
